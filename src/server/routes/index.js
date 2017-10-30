9baccd58344627ddc4314b1801ddec5525b69783
Karol Wojdyła <k4wo@outlook.com>
2017-10-30 19:32:47 +0100

    serve static

---
+++ src/server/routes/index.js
@@ -0,0 +1,13 @@
+const { join: pathJoin } = require('path')
+const Router = require('koa-router')
+const serveStatic = require('koa-static')
+
+module.exports = ({ app, middleware }) => {
+  const router = new Router({
+    prefix: '/'
+  })
+
+  app.use(serveStatic(pathJoin(__dirname, '../../../dist')))
+  app.use(router.routes())
+  app.use(router.allowedMethods())
+}
