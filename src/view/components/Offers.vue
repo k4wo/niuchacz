0f1a6a3e7c69a5cb8c5992f9c07f0d7ca895ff16
Karol Wojdyła <k4wo@outlook.com>
2017-12-02 15:13:35 +0100

    TopBar as separate component

--- src/view/components/Offers.vue
+++ src/view/components/Offers.vue
@@ -1,15 +1,12 @@
 <template>
   <ul>
-    <li class="top-bar">
-      <span>Na liście znajduje się {{offers.length}} ofert.</span>
-      <button @click="selectAll">Zaznacz wszystkie</button>
-      <button @click="toggleModal">Filtruj po cenie</button>
-      <price-filter v-if="isModalPriceActive" :onSave="onFilter"></price-filter>
-      <label>
-        Pokaż ulubione oferty
-        <input type="checkbox" v-model="isFavouriteMarked" @change="showFavourite(isFavouriteMarked)"/>
-      </label>
-    </li>
+    <top-bar
+      :offerCounter="offers.length"
+      :saveAsRead="saveAsRead"
+      :showFavourite="showFavourite"
+      :priceFilter="priceFilter"
+      :selectAll="selectAll">
+    </top-bar>
     <offerslot 
       v-for="offer in offers" 
       :key="offer.id" 
@@ -22,7 +19,7 @@
 
 <script>
 import offerslot from "./OfferSlot.vue";
-import priceFilter from "./PriceFilter.vue";
+import topBar from "./TopBar.vue";
 
 export default {
   props: {
@@ -32,47 +29,24 @@ export default {
     showFavourite: Function,
     priceFilter: Function
   },
-  methods: {
+
+methods: {
     selectAll() {
       this.offers.forEach(offer => {
         this.$set(offer, "markAsRead", true);
-        this.saveAsRead(offer._id)
+        this.saveAsRead(offer._id);
       });
-    },
-    onFilter(price) {
-      this.isModalPriceActive = false
-      this.priceFilter(+price)
-    },
-    toggleModal() {
-      this.isModalPriceActive = !this.isModalPriceActive
     }
   },
-  data() {
-    return {
-      isFavouriteMarked: false,
-      isModalPriceActive: false
-    }
-  },
+
   components: {
     offerslot,
-    priceFilter
+    topBar
   }
 };
 </script>
 
 <style scoped>
-.top-bar {
-  background: #374a5e;
-  padding: 10px;
-  color: #fff;
-  margin: -1px 1px;
-  display: flex;
-  justify-content: space-between;
-  position: relative;
-}
-.top-bar label {
-  cursor: pointer;
-}
 ul {
   grid-column: 2 / 3;
   grid-row: 2 / 3;
---
+++ src/view/components/TopBar.vue
@@ -0,0 +1,59 @@
+<template>
+    <li class="top-bar">
+      <label class="selectAll">
+        <input type="checkbox" v-model="checkAll" @change="selectAll"/>
+        <span>Na liście znajduje się {{offerCounter}} ofert.</span>
+      </label>
+
+      <price-filter :onSave="priceFilter"></price-filter>
+
+      <label>
+        Pokaż ulubione oferty
+        <input 
+          type="checkbox"
+          v-model="isFavouriteMarked"
+          @change="showFavourite(isFavouriteMarked)"/>
+      </label>
+    </li>
+</template>
+
+<script>
+import priceFilter from "./PriceFilter.vue";
+
+export default {
+  props: {
+    selectAll: Function,
+    offerCounter: Number,
+    saveAsRead: Function,
+    priceFilter: Function,
+    showFavourite: Function
+  },
+
+  data() {
+    return {
+      isFavouriteMarked: false,
+      checkAll: false
+    };
+  },
+
+  components: {
+    priceFilter
+  }
+};
+</script>
+
+<style scoped>
+.top-bar {
+  background: #374a5e;
+  padding: 10px;
+  color: #fff;
+  margin: -1px 1px;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+}
+.top-bar label, .selectAll {
+  cursor: pointer;
+}
+</style>
+
